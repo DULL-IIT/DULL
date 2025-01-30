430d71cd42215cf22f0e45cc49e08ecf4872d6c1
DULL-IIT <sumon@iit.du.ac.bd>
2025-01-30 22:31:16 +0600

    Image  loading

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: "export", // Enables static site export
-    basePath: "/DULL", // Replace with your GitHub repo name
-    assetPrefix: "/DULL/",
-    images: {
-        unoptimized: true, // Ensures images load properly in static export
-      }, // Helps with serving static assets
-  };
-  
-  export default nextConfig
-  
+const nextConfig = {};
+
+export default nextConfig;
